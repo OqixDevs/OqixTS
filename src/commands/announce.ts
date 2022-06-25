4fed5838892b6d3b7be8919a4feabe49e971bc70
xvanick1 <jozef.vanicky@gmail.com>
2022-06-25 10:41:08 +0300

    feat: new command announce

---
+++ src/commands/announce.ts
@@ -0,0 +1,43 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import { PermissionFlagsBits } from 'discord-api-types/v9';
+
+/**
+ * Makes an announcement into a channel.
+ */
+export const data = new SlashCommandBuilder()
+    .setName('announce')
+    .setDescription('Announces a message to a channel.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addStringOption((option) =>
+        option
+            .setName('message')
+            .setDescription('Your message to be announced.')
+            .setRequired(true)
+    );
+
+export async function execute(interaction: CommandInteraction) {
+    const message = interaction.options.getString('message');
+
+    if (!message) {
+        return interaction.reply({
+            content: 'You need to input a message.',
+            ephemeral: true,
+        });
+    }
+
+    if (message.length > 2000) {
+        return interaction.reply({
+            content: 'Message is too long.',
+            ephemeral: true,
+        });
+    }
+
+    return interaction.reply(message).catch((error: unknown) => {
+        console.error(error);
+        interaction.reply({
+            content: 'There was an error trying to announce in this channel!',
+            ephemeral: true,
+        });
+    });
+}
