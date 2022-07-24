a96a0b90e8e72c1d166c5636c136192090166ba8
Jaroslav Hort <hortjaroslav0@gmail.com>
2022-07-24 19:27:37 +0200

    feat: setup subject channels if they aren't set up on invocation

--- src/buttons/addChannel.ts
+++ src/buttons/addChannel.ts
@@ -1,5 +1,6 @@
 import { ButtonInteraction, MessageActionRow, MessageButton } from 'discord.js';
 import { channelSelect } from '../selects';
+import { setupSubjectChannels } from '../utils';
 
 export const data = new MessageActionRow<MessageButton>().addComponents(
     new MessageButton()
@@ -9,6 +10,22 @@ export const data = new MessageActionRow<MessageButton>().addComponents(
 );
 
 export async function execute(interaction: ButtonInteraction) {
+    if (!interaction.guild) {
+        // I don't think this should happen, but apparently guild can be null?
+        return;
+    }
+
+    if (channelSelect.data.length < 1) {
+        setupSubjectChannels(interaction.guild);
+        if (channelSelect.data.length < 1) {
+            await interaction.reply({
+                content:
+                    'Failed to fetch subject channels, please contant administrator',
+            });
+            return;
+        }
+    }
+
     interaction.reply({
         content: 'Select channels you want to add or remove below.',
         components: [...channelSelect.data],
