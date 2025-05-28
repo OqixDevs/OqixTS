4df279ea2bc9add7256aefae81c14a3c117bb8b9
Michal Drla <michal.drla@protonmail.com>
2025-05-28 18:45:30 +0200

    fix: Fix parsing of the thesis site (#125)
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- src/utils/scrapeThesis.ts
+++ src/utils/scrapeThesis.ts
@@ -24,7 +24,7 @@ export async function scrapeThesis(bachelorThesisPath: string) {
     const $ = load(response.data);
     console.log('LOG: Parsing author name from the page.');
     const authorName = $(
-        'ds-metadata-representation-list.ds-item-page-mixed-author-field:nth-child(4) > ds-metadata-field-wrapper:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ds-metadata-representation-loader:nth-child(1) > ds-plain-text-metadata-list-element:nth-child(1) > div:nth-child(1) > a:nth-child(1)'
+        'ds-metadata-representation-list.ds-item-page-mixed-author-field:nth-child(4) > ds-metadata-field-wrapper:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ds-metadata-representation-loader:nth-child(1) > ds-plain-text-metadata-list-element:nth-child(1) > div:nth-child(1) > span:nth-child(1)'
     ).text();
     console.log(`LOG: Parsed author name from the page ${authorName}.`);
     return authorName
